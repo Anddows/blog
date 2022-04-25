87b66d23f21040e30a087b452025f06385826eb8
k1ki <84444286+Anddows@users.noreply.github.com>
2022-04-25 22:29:14 +0500

    tayyor

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,27 @@
 from django.urls import path
-from .views import ( BlogListView,
+from .views import ( AboutPageView,
+                     BlogListView,
                      BlogDetailView,
                      BlogCreateView,
                      BlogUpdateView,
-BlogDeleteView
+                     CcsPageView,
+                     SendOrderPageView,
+                     ProjectsPageView,
+                     ServicePageView,
+                     SplashScreenPageView,
+BlogDeleteView,
                      )
 
 urlpatterns = [
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name = 'post-delete'),
     path('post/<int:pk>/edit', BlogUpdateView.as_view(), name = 'post-edit'),
     path('blog/news/', BlogCreateView.as_view(), name = 'post-new'),
-    path('', BlogListView.as_view(), name = 'home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name = "post-detail")
+    path('home/', BlogListView.as_view(), name = 'home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name = "post-detail"),
+    path('about/', AboutPageView.as_view(), name = "about"),
+    path('projects/', ProjectsPageView.as_view(), name = "projects"),
+    path('buyurtma/', SendOrderPageView.as_view(), name = "buyurtma"),
+    path('ccs/', CcsPageView.as_view(), name = "ccs"),     
+    path('', SplashScreenPageView.as_view(), name = 'splash-screen'),
+    path('service/', ServicePageView.as_view(), name = 'service')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from re import template
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
@@ -27,6 +28,24 @@ class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post-delete.html'
     success_url = reverse_lazy('home')
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ProjectsPageView(TemplateView):
+    template_name = 'proj.html'
+
+class CcsPageView(TemplateView):
+    template_name = 'ccs.html'
+
+class SendOrderPageView(TemplateView):
+    template_name = 'buyurtma.html' 
+
+class ServicePageView(TemplateView):
+    template_name = 'services.html'
+
+class SplashScreenPageView(TemplateView):
+    template_name = 'index.html'
 
 
 
